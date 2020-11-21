86cdff0bb80747a69171134624551e169cab588d
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2020-11-21 14:15:07 -0500

    first commit

---
+++ Unsolved/script.js
@@ -0,0 +1,20 @@
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+function renderTodos() {
+    for (var i = 0; i < todos.length; i++) {
+        //create li element
+        var li = document.createElement('li')
+        //add todo text to li
+        li.innerText = todos[i]
+        //append li to todolist
+        todoList.appendChild(li)g
+    }
+    
+}
+
+renderTodos()
